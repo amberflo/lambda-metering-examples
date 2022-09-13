9fa6594a99c0ba9f141814bc2c4e051992d22a14
Filipe Correia <filipe@amberflo.io>
2022-09-13 14:02:59 -0300

    Add test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+api_url=''
+
+methods=(
+    cw-subscriber
+    direct-api
+    direct-s3
+    direct-sqs
+)
+
+for m in "${methods[@]}"
+do
+    curl -X POST "$api_url/$m"
+done
